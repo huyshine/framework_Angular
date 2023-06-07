2d98bc09aac587872dbb90a51c0292b01b242980
huyshine <huyshine1111@gmail.com>
2023-06-07 11:07:50 +0700

    install  yup

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -4,6 +4,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthentionService } from 'src/app/API/Users/authention.service';
 import { Router } from '@angular/router';
 import { ToastService } from 'angular-toastify';
+import * as yup from 'yup';
+// import { yupResolver } from "@hookform/resolvers/yup";
+import { FormsModule } from '@angular/forms';
+
+
 
 // import { }
 @Component({
@@ -17,11 +22,25 @@ export class LoginComponent {
     password: '',
   };
 
+  schema = yup.object({
+    email: yup
+      .string()
+      .required("* Please enter your email")
+      .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid email"),
+    password: yup
+      .string()
+      .required("* Please enter a password")
+      .min(6, "* At least 6 characters")
+      .trim(),
+  });
   
   constructor(
     private authService: AuthentionService,
     private router: Router,
     private toastService: ToastService,
+    private fb: FormBuilder,
+
+
 
 
      ) { }
@@ -29,34 +48,38 @@ export class LoginComponent {
      
 
   onSubmit() {
-    // console.log('Registration submitted:', this.credentials);
-    // Goi API
+    let errors : any = this.schema.validate(this.credentials);
 
-    this.authService.signin(this.credentials).subscribe(
+    if (errors.length > 0) {
+      // Handle errors
+      // this.toastService.error("Error! Incorrect account or password");
+    } else {
+      // Input is valid
+      this.authService.signin(this.credentials).subscribe(
+        (response) => {
+          this.authService.setTocken(response.accessToken);
+          console.log(this.authService.getTocken());
+          console.log('Login successful');
+          localStorage.setItem("user", JSON.stringify(response));
+          this.toastService.success("Logged in successfully");
+          
+          // toast.success("Logged in successfully");
+          // const roleId = ;
+          setTimeout(() => response.data.role == "member" ? this.router.navigate(['/home']) : this.router.navigate(['/admin']), 2000);
+  
+          // Xử lý thành công, chẳng hạn chuyển hướng đến trang đăng nhập
+        },
+        (error) => {
+          // console.log('Login failed');
+          this.toastService.error("Error! Incorrect account or password");
+  
+          // Xử lý lỗi, hiển thị thông báo lỗi cho người dùng
+        }
+      );
+    }
+    
+    
 
-      
-      (response) => {
-
-        this.authService.setTocken(response.accessToken);
-        console.log(this.authService.getTocken());
-        
-        console.log('Login successful');
-        localStorage.setItem("user", JSON.stringify(response));
-        this.toastService.success("Logged in successfully");
-        
-        // toast.success("Logged in successfully");
-        // const roleId = ;
-        setTimeout(() => response.data.role == "member" ? this.router.navigate(['/home']) : this.router.navigate(['/admin']), 2000);
-
-        // Xử lý thành công, chẳng hạn chuyển hướng đến trang đăng nhập
-      },
-      (error) => {
-        // console.log('Login failed');
-        this.toastService.error("Error! Incorrect account or password");
-
-        // Xử lý lỗi, hiển thị thông báo lỗi cho người dùng
-      }
-    );
   }
 
 
